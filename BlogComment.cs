9a573c073e51132f2b8e613aa873abab5fc5c022
Artur Drobinskiy <artur.drobinskiy@gmail.com>
2021-10-21 12:23:53 +0700

    added a couple more query examples

--- BlogComment.cs
+++ BlogComment.cs
@@ -7,15 +7,18 @@ namespace Blog
         public int Id { get; set; }
         public string Text { get; set; }
 
-        public BlogComment(string text, DateTime createdDate)
+        public BlogComment(string text, DateTime createdDate, string userName)
         {
             Text = text;
             CreatedDate = createdDate;
+            UserName = userName;
         }
 
         public DateTime CreatedDate { get; set; }
 
         public BlogPost BlogPost { get; set; }
         public int BlogPostId { get; set; }
+        
+        public string UserName { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -19,7 +19,38 @@ namespace Blog
             InitializeData(context);
 
             var data = context.BlogPosts.Select(x => x.Title).ToList();
+
+            Console.WriteLine("How many comments each user left:");
+            //ToDo: write a query and dump the data to console
+            // Expected result (format could be different, e.g. object serialized to JSON is ok):
+            // Ivan: 4
+            // Petr: 2
+            // Elena: 3
+
+            Console.WriteLine("Posts ordered by date of last comment:");
+            //ToDo: write a query and dump the data to console
+            // Expected result (format could be different, e.g. object serialized to JSON is ok):
+            // Post2: '2020-03-06'
+            // Post1: '2020-03-05'
+            // Post3: '2020-02-14'
+
+
+            Console.WriteLine("How many last comments each user left:");
+            // 'last comment' is the latest Comment in each Post
+            //ToDo: write a query and dump the data to console
+            // Expected result (format could be different, e.g. object serialized to JSON is ok):
+            // Ivan: 2
+            // Petr: 1
+
             
+            // Console.WriteLine(
+            //     JsonSerializer.Serialize(BlogService.NumberOfCommentsPerUser(context)));
+            // Console.WriteLine(
+            //     JsonSerializer.Serialize(BlogService.PostsOrderedByLastCommentDate(context)));
+            // Console.WriteLine(
+            //     JsonSerializer.Serialize(BlogService.NumberOfLastCommentsLeftByUser(context)));
+
+
             Console.WriteLine(JsonSerializer.Serialize(data));
         }
 
@@ -29,25 +60,27 @@ namespace Blog
             {
                 Comments = new List<BlogComment>()
                 {
-                    new BlogComment("1", new DateTime(2020, 3, 4)),
-                    new BlogComment("2", new DateTime(2020, 3, 1)),
+                    new BlogComment("1", new DateTime(2020, 3, 2), "Petr"),
+                    new BlogComment("2", new DateTime(2020, 3, 4), "Elena"),
+                    new BlogComment("8", new DateTime(2020, 3, 5), "Ivan"),
                 }
             });
             context.BlogPosts.Add(new BlogPost("Post2")
             {
                 Comments = new List<BlogComment>()
                 {
-                    new BlogComment("3", new DateTime(2020, 3, 5)),
-                    new BlogComment("4", new DateTime(2020, 3, 6)),
+                    new BlogComment("3", new DateTime(2020, 3, 5), "Elena"),
+                    new BlogComment("4", new DateTime(2020, 3, 6), "Ivan"),
                 }
             });
             context.BlogPosts.Add(new BlogPost("Post3")
             {
                 Comments = new List<BlogComment>()
                 {
-                    new BlogComment("5", new DateTime(2020, 2, 7)),
-                    new BlogComment("6", new DateTime(2020, 2, 9)),
-                    new BlogComment("7", new DateTime(2020, 2, 8)),
+                    new BlogComment("5", new DateTime(2020, 2, 7), "Ivan"),
+                    new BlogComment("6", new DateTime(2020, 2, 9), "Elena"),
+                    new BlogComment("7", new DateTime(2020, 2, 10), "Ivan"),
+                    new BlogComment("9", new DateTime(2020, 2, 14), "Petr"),
                 }
             });
             context.SaveChanges();
